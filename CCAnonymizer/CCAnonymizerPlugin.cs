17afc266afda0189c460ee489421c3fda83f5d3c
Spider <29214314+spide-r@users.noreply.github.com>
2025-05-10 10:22:50 -0500

    Project start

---
+++ CCAnonymizer/CCAnonymizerPlugin.cs
@@ -0,0 +1,44 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace CCAnonymizer;
+
+public sealed class CCAnonymizerPlugin : IDalamudPlugin
+{
+
+    private const string CommandName = "/panon";
+
+    public readonly WindowSystem WindowSystem = new("CCAnon");
+    public CCAnonymizerPlugin(IDalamudPluginInterface pluginInterface,
+        ICommandManager commandManager)
+    {
+        
+
+        pluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUI;
+        pluginInterface.UiBuilder.Draw += DrawUI;
+
+    }
+
+    private void ToggleConfigUI()
+    {
+    }
+
+    private void DrawUI()
+    {
+        WindowSystem.Draw();
+    }
+
+
+    private void OnCommand(string command, string arguments)
+    {
+    }
+
+    public void Dispose()
+    {
+        WindowSystem.RemoveAllWindows();
+
+
+    }
+}
